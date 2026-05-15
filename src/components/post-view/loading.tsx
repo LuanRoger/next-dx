602c375c9e3aaef645ac4083b8c3a44c61a0cf3f
ROG <luan.roger.2003@gmail.com>
2026-05-15 18:21:22 -0300

    feat: set a absolute size for skeleton

--- src/components/post-view/loading.tsx
+++ src/components/post-view/loading.tsx
@@ -1,5 +1,10 @@
 import { Skeleton } from "../ui/skeleton";
 
 export default function PostViewLoading() {
-  return <Skeleton className="h-96 w-full" />;
+  return (
+    <div className="flex flex-col gap-2">
+      <Skeleton className="h-5 w-46" />
+      <Skeleton className="h-96 w-full" />
+    </div>
+  );
 }
