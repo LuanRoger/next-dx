14ce809c6cb7fc75cea5a76edc1cfbe172986638
ROG <luanroger.dev@gmail.com>
2026-07-04 08:59:51 -0300

    feat: add more validations to form

--- src/components/user-id-form/schemas/index.ts
+++ src/components/user-id-form/schemas/index.ts
@@ -1,7 +1,15 @@
 import z from "zod";
 
 export const formSchema = z.object({
-  userId: z.string().optional(),
+  userId: z
+    .string()
+    .optional()
+    .refine(
+      (val) => val === undefined || val === "" || !Number.isNaN(Number(val)),
+      {
+        message: "User ID must be a valid number",
+      }
+    ),
 });
 
 export type FormSchema = z.infer<typeof formSchema>;
